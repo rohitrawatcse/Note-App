4056ac5693ddd10a7d81ce7d9c26cc3f0ec29dd5
thegolumehra <golucodes@gmail.com>
2022-09-25 18:44:05 +0530

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# Notee-App
--- script.js
+++ script.js
@@ -1,76 +1,47 @@
+// const noteText = document.querySelector("#note");
+// 
+// function underlineNote(){
+    // const note = document.querySelector("#note");
+    // let text = note.value
+    // let selection = window.getSelection();
+    // let boldText = "<u>" + selection + "</u>"
+    // note.value = text.replace(selection, boldText)   
+// }
+const note = document.querySelector("#note");
+const allnote = document.querySelector(".allnote");
 
-function copyNote(){
-    
-    // const title = document.querySelector("#title")
-    const note = document.querySelector("#note")
-    const copSelection = window.getSelection();
-    // // Select the text field
-    // title.select();
-    // title.setSelectionRange(0, 99999); // For mobile devices
-    note.select();
-    note.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-//   navigator.clipboard.writeText(title);
-   navigator.clipboard.writeText(note.value);
-  alert("Copied the text: " + note.value);
-}
-
-function boldNote(){
-    // const title = document.querySelector("#title")
-    const note = document.querySelector("#note")
-    // const boldSelection = window.getSelection();
-   
-    let text = note.value
-    let selection = window.getSelection();
-    let boldText = "<b>" + selection + "</b>"
-    note.value = text.replace(selection, boldText) 
-  
-}
-
-function underlineNote(){
-    // const title = document.querySelector("#title")
-    const note = document.querySelector("#note")
-    // const boldSelection = window.getSelection();
-   
-    let text = note.value
-    let selection = window.getSelection();
-    let boldText = "<u>" + selection + "</u>"
-    note.value = text.replace(selection, boldText) 
-  
-}
-
-
+const copyBtn = document.querySelector(".copy-btn");
 
 function addNote(){
-    
-    // const title = document.querySelector("#title").value
-const note = document.querySelector("#note").value
-const allnote = document.querySelector(".allnote")
-    const titleText = document.querySelector(".title-text")
-    const noteText = document.querySelector(".note-text")
-    
-    // <!--<h4 class="title-text">${title}</h4>-->
     let card = `<div class="note">
-                    
-                    <p class="note-text">${note}</p>
+                    <p class="note-text">${note.value}</p>
+                    <button class="copy-btn"><img src="https://img.icons8.com/material-sharp/24/000000/copy.png"/></button>
                 </div>`;
     allnote.innerHTML += card;
-    // title.select();
-    // note.select();
-    // title.setSelectionRange(0, 99999);
-    // note.setSelectionRange(0, 99999);
-    // titleText.textContent = title.value;
-    // noteText.textContent = note.value;
-    
-    // title.value = "";
     note.value = "";
-
-    
-
-
 }
 
+// function copyNote() {
+let cards = allnote.childNodes;
+console.log(cards)
+cards.forEach( function cardList(current){
+    // const cp = current.childNodes[3].getAttribute('class');
+    copyBtn.addEventListener("click",function nope(){
+        console.log("hello");
+    })
+})
+    // const noteText = document.querySelector(".note-text");
+    // navigator.clipboard.writeText(noteText.textContent);
+// }
+// function boldNote(){
+    // const note = document.querySelector("#note")
+// 
+    // let text = note.value;
+    // let selection = window.getSelection();
+    // let boldText = "<b>" + selection + "</b>"
+    // note.value = text.replace(selection, boldText) 
+// 
+// }
 
 
 
@@ -119,14 +90,16 @@ const allnote = document.querySelector(".allnote")
 
 
 
-$("#search-icon").click(function() {
-    $(".nav").toggleClass("search");
-    $(".nav").toggleClass("no-search");
-    $(".search-input").toggleClass("search-active");
-  });
-  
-  $('.menu-toggle').click(function(){
-     $(".nav").toggleClass("mobile-nav");
-     $(this).toggleClass("is-active");
-  });
-  
+
+
+// $("#search-icon").click(function() {
+    // $(".nav").toggleClass("search");
+    // $(".nav").toggleClass("no-search");
+    // $(".search-input").toggleClass("search-active");
+//   });
+//   
+//   $('.menu-toggle').click(function(){
+    //  $(".nav").toggleClass("mobile-nav");
+    //  $(this).toggleClass("is-active");
+//   });
+//   
