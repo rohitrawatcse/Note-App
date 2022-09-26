faf8705d889699a8347661f111dce4da2f26e6e0
thegolumehra <golucodes@gmail.com>
2022-09-26 12:51:04 +0530

    added title

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const note = document.querySelector("#note");
+const title = document.querySelector("#title")
 const allnote = document.querySelector(".allnote");
 
 function addNote(){
     let card = `<div class="note">
-                    <p class="note-text">${note.value}</p>
-                    <button class="copy-btn"><img src="https://img.icons8.com/material-sharp/24/000000/copy.png"/></button>
+                    <div>
+                        <p class="title-text">${title.value}</p>
+                        <button class="copy-btn"><img src="https://img.icons8.com/material-sharp/24/000000/copy.png"/></button>
+                    </div>
+                    <div>
+                        <p class="note-text">${note.value}</p>
+                        <button class="copy-btn"><img src="https://img.icons8.com/material-sharp/24/000000/copy.png"/></button>
+                    </div>
                 </div>`;
     if (note.value=="") {
         note.value ="note"
@@ -12,6 +19,7 @@ function addNote(){
         allnote.innerHTML += card;
     }
     note.value = "";
+    title.value ="";
 
     var copyBtn = document.querySelectorAll(".copy-btn")
     var copyArr = [...copyBtn]
@@ -20,6 +28,7 @@ function addNote(){
     })
     function copyNote(btn) {
         const element = btn.currentTarget.parentElement;
+        console.log(element.textContent)
         navigator.clipboard.writeText(element.textContent);
     }
 }
@@ -27,69 +36,23 @@ function addNote(){
 function boldNote(){
     let text = note.value;
     let selection = window.getSelection().toString();
-    let boldText = "<b>" + selection + "</b>"
-    note.value = text.replace(selection, boldText) 
-
+    if (text != "") {
+        if (selection == "") {
+            selection = text
+        }
+        let boldText = "<b>"+ selection +"</b>"
+        note.value = text.replace(selection, boldText)
+    }
 }
 
-function underlineNote(){
-    let text = note.value
-    let selection = window.getSelection();
-    let underText = "<u>" + selection + "</u>"
-    note.value = text.replace(selection, underText)   
+function underLineNote(){
+    let text = note.value;
+    let selection = window.getSelection().toString();
+    if (text != "") {
+        if (selection == "") {
+            selection = text
+        }
+        let underText = "<u>"+ selection +"</u>"
+        note.value = text.replace(selection, underText)
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $("#search-icon").click(function() {
-    // $(".nav").toggleClass("search");
-    // $(".nav").toggleClass("no-search");
-    // $(".search-input").toggleClass("search-active");
-//   });
-//   
-//   $('.menu-toggle').click(function(){
-    //  $(".nav").toggleClass("mobile-nav");
-    //  $(this).toggleClass("is-active");
-//   });
-//   
